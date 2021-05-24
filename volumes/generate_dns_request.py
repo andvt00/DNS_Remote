f0412e61c20f3eb871406e7616daf30b7265eaee
thanhan17 <andoan1720@gmail.com>
2021-05-25 02:07:07 +0700

    ver 2

--- volumes/generate_dns_request.py
+++ volumes/generate_dns_request.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from scapy.all import*
 # Construct the DNS header and payload
-Qdsec  = DNSQR(qname='www.example.com')
+Qdsec  = DNSQR(qname='duong.example.com')
 dns    = DNS(id=0xAAAA, qr=0, qdcount=1, ancount=0, nscount=0,arcount=0, qd=Qdsec)
 # Construct the IP, UDP headers, and the entire packet
-ip  = IP(dst='10.9.0.5', src='10.9.0.153')
-udp = UDP(dport=33333, sport=53, chksum=0)
+ip  = IP(dst='10.9.0.5', src='192.168.1.6')
+udp = UDP(dport=40000, sport=33333, chksum=0)
 request = ip/udp/dns
 # Save the packet to a file
 with open('ip_req.bin', 'wb') as f:
